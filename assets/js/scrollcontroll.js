974d5395663dc004d9a68db2c7d58c267f7f11fa
Marcos Brendon De Paula <marcos.brendon@ufms.br>
2022-07-07 16:02:19 -0300

    📋✅ first commit

---
+++ assets/js/scrollcontroll.js
@@ -0,0 +1,43 @@
+ScrollReveal({ reset: true });
+
+ScrollReveal().reveal(".show-once", {
+	reset: false
+});
+
+ScrollReveal().reveal(".top_animation", {
+	duration: 1300,
+	origin: "top",
+	distance: "400px",
+	easing: "cubic-bezier(0.5, 0, 0, 1)",
+	rotate: {
+		x: 20,
+		z: -10
+	}
+});
+
+ScrollReveal().reveal(".left_animation", {
+    duration: 1000,
+    origin: "left",
+    distance: "400px",
+    easing: "cubic-bezier(0.5, 0, 0, 1)",
+    rotate: {
+        x: 20,
+        z: -10
+    }
+});
+
+ScrollReveal().reveal(".right_animation", {
+    duration: 1000,
+    origin: "right",
+    distance: "400px",
+    easing: "cubic-bezier(0.5, 0, 0, 1)",
+    rotate: {
+        x: 20,
+        z: -10
+    }
+});
+  
+ScrollReveal().reveal(".fade_in_animation", {
+	duration: 1000,
+	move: 0
+});
